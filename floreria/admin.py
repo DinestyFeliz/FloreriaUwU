c4c157a2c6c861baa5d661b090d6e1e57eb6ea1c
unknown <st.sanzana@alumnos.duoc.cl>
2019-12-04 12:21:49 -0300

    Carrito,Limpiar Carrito y Ticket  Admin Django

--- floreria/admin.py
+++ floreria/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Categoria, Flores
+from .models import Categoria, Flores, Ticket
 
 # Register your models here.
 
 admin.site.register(Categoria)
 admin.site.register(Flores)
+admin.site.register(Ticket)
